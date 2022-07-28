f5ad2145deae567c24febb9922a723698744108c
Lorena Araujo Almeida <87384833+lorenaaraujoo@users.noreply.github.com>
2022-07-28 15:34:28 -0300

    Exercício:
    Elabore uma superclasse chamada figura que possua o seguinte atributo
    *área, e os métodos, exibirArea() e calculaArea();
    E as classes filhas quadrado, triângulo, losango e retângulo fazendo as devidas sobrescritas, crie uma classe principal e instancie cada uma das classes filhas.

---
+++ Revision-geometry/src/app/Triangle.java
@@ -0,0 +1,24 @@
+package app;
+
+public class Triangle extends Figure {
+
+	public Triangle(double area) {
+		super(area);
+	}
+	
+	@Override
+	public void displayArea() {
+		System.out.println("------Triangle------");
+		System.out.println("Área initial: "+getArea());
+	}
+	
+	@Override
+	public void calcArea(double base, double height) {
+		double result = 0;
+		result = (base * height) / 2;
+		System.out.println("The area of ​​the given triangle is "+result);
+		System.out.println();
+	}
+	
+	
+}
